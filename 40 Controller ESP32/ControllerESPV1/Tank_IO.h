b26f6b1b17485ab7fdc419656c41945dcf7c3091
Mark Hancock <engineer@hancockhome.com>
2018-08-26 17:26:08 -0700

    Change IO to match Andrea's HW

--- 40 Controller ESP32/ControllerESPV1/Tank_IO.h
+++ 40 Controller ESP32/ControllerESPV1/Tank_IO.h
@@ -15,11 +15,11 @@ SDA : 21
 #define OLED_SDA 21
 
 //IO
-#define pwm_motor1 13
-#define pwm_motor2 25
-#define cw_motor1  14
+#define pwm_motor1 12
+#define pwm_motor2 14
+#define cw_motor1  13
 #define cw_motor2  27
-#define ccw_motor1 12
+#define ccw_motor1 25
 #define ccw_motor2 26
 
 
