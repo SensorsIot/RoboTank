14edafd1e4a27530d46906c300e6e128d352041a
Mark Hancock <engineer@hancockhome.com>
2018-09-02 12:44:38 -0700

    Correct IO error

--- 40 Controller ESP32/ControllerESPV1/Tank_IO.h
+++ 40 Controller ESP32/ControllerESPV1/Tank_IO.h
@@ -16,11 +16,11 @@ SDA : 21
 
 //IO
 #define pwm_motor1 13
-#define pwm_motor2 25
 #define A2 14 //cw_motor1
-#define A1 27 //cw_motor2
 #define B2 12 //ccw_motor1
-#define B1 16 //ccw_motor2
+#define pwm_motor2 25
+#define A1 27 //cw_motor2
+#define B1 26 //ccw_motor2
 
 //MAC for the Remote Controller
 #define REMOTE_MAC {0x30, 0xAE, 0xA4, 0x1A, 0x19, 0x08}
