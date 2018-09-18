879772cbe9881e9e85ffb4f803d5dca13af19cdd
Mark Hancock <engineer@hancockhome.com>
2018-09-17 20:52:48 -0700

    Re-enable SetMotorSpeed call commented out for debugging

--- 40 Controller ESP32/ControllerESPV1/ControllerESPV1.ino
+++ 40 Controller ESP32/ControllerESPV1/ControllerESPV1.ino
@@ -100,7 +100,7 @@ void OnDataSent(const uint8_t *mac_addr, esp_now_send_status_t status)
 void loop()
 {
   telemetry.batteryVoltage = getBatteryVoltage();
-  //setMotorSpeed(roboSpeed, roboAngle);
+  setMotorSpeed(roboSpeed, roboAngle);
 
 #if HasDisplay
   if (millis() - lastDisplay > displayUpdate_ms) {
